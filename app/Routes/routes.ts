2b36db67fd25c6070bd15686129a1c56854c4b2e
roy.goldhar <roy@goldhar.net>
2025-07-14 12:21:24 +0300

    Backend changed to typescript, frontend pagination box improvement

--- app/Routes/routes.ts
+++ app/Routes/routes.ts
@@ -1,23 +1,27 @@
-
 import type { Application, Router } from 'express';
+import express from 'express';
 import signUp from '../Controllers/sign-up.js';
 import validateCode from '../Controllers/validate.js';
 import login from '../Controllers/login.js';
 import getBalance from '../Controllers/balance.js';
-import {makeTransaction} from '../Controllers/transactions.js';
-import {getTransactions} from '../Controllers/transactions.js';
-import {logout} from '../Controllers/logout.js';
+import { makeTransaction, getTransactions } from '../Controllers/transactions.js';
+import { logout } from '../Controllers/logout.js';
 import cors from 'cors';
 
-const prefix = 'api'
-export default function addRoutes(app : Application) {
-    app.use(prefix, (router : Router) => {
-        router.use('/auth/sign-up', signUp());
-        router.use('/auth/sign-up/validate', validateCode());
-        router.use('/auth/login', login());
-        router.use('/balances', getBalance());
-        router.use('/transactions', getTransactions());
-        router.use('/transactions', makeTransaction());
-        router.use('/logout', logout());
-    });
+const prefix = '/api';
+
+export default function addRoutes(app: Application) {
+  app.use(cors({ origin: process.env.WEB_URL, credentials: true }));
+
+  const router: Router = express.Router();
+
+  router.use('/auth/sign-up', signUp());
+  router.use('/auth/sign-up/validate', validateCode());
+  router.use('/auth/login', login());
+  router.use('/balances', getBalance());
+  router.use('/transactions', getTransactions());
+  router.use('/transactions', makeTransaction());
+  router.use('/logout', logout());
+
+  app.use(prefix, router);
 }
