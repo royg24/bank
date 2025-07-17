e85abc79cb54773caf7935f9afacafc8376e1669
roy.goldhar <roy@goldhar.net>
2025-07-17 09:55:46 +0300

    redis is running on redis cloud

--- app/Controllers/logout.ts
+++ app/Controllers/logout.ts
@@ -7,11 +7,14 @@ import { AuthenticationError } from '../errorHandler.js';
 const client = redis.createClient({
   socket: {
     host: process.env.REDIS_HOST || '127.0.0.1',
-    port: 6379
-  }
+    port: process.env.REDIS_PORT ? Number(process.env.REDIS_PORT) : 6379,
+    tls: false,
+  },
+  username: process.env.REDIS_USERNAME,
+  password: process.env.REDIS_PASSWORD,
 });
-
 await client.connect();
+
 
 export function logout() {
     const router = Router();
