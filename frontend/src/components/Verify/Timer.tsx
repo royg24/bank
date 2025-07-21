7c0711f235635511b50d6761385396daa981a1fc
roy.goldhar <roy@goldhar.net>
2025-07-21 12:20:49 +0300

    improve front and fix set balance problem

--- frontend/src/components/Verify/Timer.tsx
+++ frontend/src/components/Verify/Timer.tsx
@@ -1,6 +1,7 @@
 import Countdown from 'react-countdown';
 import { Box, Typography } from '@mui/material';
 import { toast } from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
 
 const renderer = ({ minutes, seconds, completed }: any) => {
   if (completed) {
@@ -16,9 +17,8 @@ const renderer = ({ minutes, seconds, completed }: any) => {
         sx={{
           fontFamily: 'monospace',
           fontWeight: 'bold',
-          fontSize: '3rem',
-          color: 'white',
-          padding: '0.5em 1em',
+          fontSize: '2.5rem',
+          color: 'white'
         }}
       >
         {minutes}:{seconds < 10 ? `0${seconds}` : seconds}
@@ -28,12 +28,19 @@ const renderer = ({ minutes, seconds, completed }: any) => {
 };
 
 export default function Timer({ expiryTimestamp }: { expiryTimestamp: number }) {
+  const navigate = useNavigate();
+
   return (
     <Box display="flex" justifyContent="center" alignItems="center">
       <Countdown
         date={expiryTimestamp}
         renderer={renderer}
-        onComplete={() => toast.info('Please sign up again')}
+        onComplete={() => {
+          toast.info('Please sign up again');
+          setTimeout(() => {
+            navigate('/access', {state: {mode: true}})
+          }, 1000);
+        }}
       />
     </Box>
   );
