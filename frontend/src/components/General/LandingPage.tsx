4c4b7bd1e6a881206a17ab7b6330e84e6aa1e7c1
roy.goldhar <roy@goldhar.net>
2025-07-23 16:11:09 +0300

    progressing in making frontend responsive

--- frontend/src/components/General/LandingPage.tsx
+++ frontend/src/components/General/LandingPage.tsx
@@ -1,82 +1,151 @@
 import { Box, Button, Typography, Card } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
-import Info from './Info'
-import { landingPageBackgroundStyle, landingPageButtonStyle, textStructure,
-     cardStructure, landingPageButtonSetStructure } from '../../css/Style';
+import Info from './Info';
+import {
+  landingPageBackgroundStyle,
+  landingPageButtonStyle,
+  textStructure,
+  cardStructure,
+  landingPageButtonSetStructure,
+} from '../../css/Style';
 
 function LandingPage() {
   const navigate = useNavigate();
 
   useEffect(() => {
     document.title = 'Gold Bank';
-  });
+  }, []);
 
   return (
-    <Box sx={landingPageBackgroundStyle}>
+    <Box
+      sx={{...landingPageBackgroundStyle,}}
+    >
 
-        <Box sx={{
-          width: '100%',
-          textAlign: 'center',
-          mb: 4,
-          marginTop: '3em',
-          maxHeight: '1em' 
-        }}>
-
-            <Typography variant="h2" sx={{ ...textStructure, fontSize: '4.5em' }}>
-                Welcome to{' '}
-
-                <Box component="span" sx={{ ...textStructure, color: 'gold' }}>
-                GoldBank
-                </Box>
-
-            </Typography>
-
-        </Box>
-
-          <Box sx={{ display: 'flex', gap: '2em', alignItems: 'flex-start', width: '90%' }}>
-
-              <Card sx={{ ...cardStructure.sx, marginTop: '0%' }}>
-
-                  <Info labelContent='🔐⚡Fast & Secure'
-                  infoContent={'Fast transactions with top-level security to keep your data safe.'} >
-                  </Info>
-
-
-              </Card>
-
-              <Card sx={{ ...cardStructure.sx, marginTop: '8%' }}>
-
-                  <Info labelContent='🌐 Reliable Service'
-                  infoContent={'Always here for you, providing seamless and consistent access.'} >
-                  </Info>
-
-              </Card>
-
-              <Card  sx={{ ...cardStructure.sx, marginTop: '16%' }}>
-
-                  <Info labelContent='💳 Easy Payments'
-                  infoContent={'Make payments quickly and effortlessly anytime, anywhere.'} >
-                  </Info>
-
-              </Card>
-
+      <Box sx={{ width: '100%', textAlign: 'center', flexShrink: 0, 
+        '@media (max-width: 600px)': {
+          marginTop: '5rem'
+        }
+       }}>
+        <Typography
+          variant="h2"
+          sx={{
+            ...textStructure,
+            fontSize: '4.5em',
+            '@media (max-width: 1400px)': {
+              fontSize: '3.5em',
+            },
+            '@media (max-width: 600px)': {
+              fontSize: '3em'
+            },
+          }}
+        >
+          Welcome to{' '}
+          <Box
+            component="br"
+            sx={{
+              display: 'none',
+              '@media (max-width: 600px)': {
+                display: 'inline',
+              },
+            }}
+          />
+          <Box
+            component="span"
+            sx={{
+              ...textStructure,
+              color: 'gold',
+              fontSize: 'inherit',
+              '@media (max-width: 1400px)': {
+                fontSize: 'inherit',
+              },
+              '@media (max-width: 600px)': {
+                fontSize: 'inherit',
+              },
+            }}
+          >
+            GoldBank
           </Box>
+        </Typography>
+      </Box>
 
-          <Box {...landingPageButtonSetStructure}>
+      <Box
+        sx={{
+          display: 'flex',
+          flexGrow: 1,
+          justifyContent: 'center',
+          flexWrap: 'nowrap',
+          flexDirection: 'column',
+          gap: 2,
+          alignItems: 'flex-start',
+          width: '150%',
 
-            <Button {...landingPageButtonStyle} onClick={
-                () => navigate('/access', {state: {mode: true}})
-                }>Sign Up
-            </Button>
+          '@media (max-width: 1400px)': {
+            flexDirection: 'column',
+            alignItems: 'center',
+            width: '120%',
+            gap: 5
+          },
 
-            <Button {...landingPageButtonStyle} onClick={
-                () => navigate('/access', {state: {mode: false}})
-                }>Log In
-            </Button>
+          '@media (max-width: 600px)' : {
+            flexDirection: 'column',
+            alignItems: 'center',
+            width: '70%',
+            gap: 5,
+          }
+        }}
+      >
+        <Card sx={{ ...cardStructure.sx, marginLeft: 0 }}>
+          <Info
+            labelContent="🔐⚡Fast & Secure"
+            infoContent="Fast transactions with top-level security to keep your data safe."
+          />
+        </Card>
 
-        </Box>
+        <Card sx={{
+          ...cardStructure.sx,
+          marginLeft: '30%',
+          '@media (max-width: 1400px)': {
+            marginLeft: 0
+          }
+        }}>
+          <Info
+            labelContent="🌐 Reliable Service"
+            infoContent="Always here for you, providing seamless and consistent access."
+          />
+        </Card>
 
+        <Card sx={{
+          ...cardStructure.sx,
+          marginLeft: '60%',
+          '@media (max-width: 1400px)': {
+            marginLeft: 0
+          } 
+        }}>
+          <Info
+            labelContent="💳 Easy Payments"
+            infoContent="Make payments quickly and effortlessly anytime, anywhere."
+          />
+        </Card>
+      </Box>
+
+      <Box
+        {...landingPageButtonSetStructure}
+      >
+        <Button
+          {...landingPageButtonStyle}
+          onClick={() => navigate('/access', { state: { mode: true } })}
+        >
+          Sign Up
+        </Button>
+
+        <Button
+          {...landingPageButtonStyle}
+          onClick={() => navigate('/access', { state: { mode: false } })}
+        >
+          Log In
+        </Button>
+      </Box>
     </Box>
   );
 }
