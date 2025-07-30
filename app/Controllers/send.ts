f1eaaae79c13a46b3cad16eab200330910da5bdb
royg24 <roy@goldhar.net>
2025-07-30 11:04:50 +0300

    change email service to gmail

--- app/Controllers/send.ts
+++ app/Controllers/send.ts
@@ -1,7 +1,7 @@
 import {Router} from 'express';
-import axios from 'axios';
 import dotenv from 'dotenv';
 import { client } from '../redis.js';
+import nodemailer from 'nodemailer';
 import { NotFoundError } from '../errorHandler.js';
 import { validateSend } from '../Validator/validations.js'
 import { normalizeEmail } from '../utils/utils.js';
@@ -21,30 +21,33 @@ export default function sendCode() {
                 throw new NotFoundError(errorMessage);
             }
 
-            const payload = {
-                sender: {
-                    name: 'Gold Bank',
-                    email: 'noreply.goldbank@gmail.com',
-                },
-                to: [{ email }],
-                subject: 'GoldBank verification code',
-                htmlContent: `<p>Welcome to GoldBank!<br>Your verification code is <strong>${code}</strong>.</p>`,
-            };
-
-            const response = await axios.post('https://api.brevo.com/v3/smtp/email', payload, {
-                headers: {
-                    'api-key': process.env.BREVO_API_KEY,
-                    'Content-Type': 'application/json',
-                },
+            const transporter = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                    user: 'noreply.goldbank@gmail.com',
+                    pass: process.env.GMAIL_APP_PASSWORD
+                }, 
+                tls: {
+                        rejectUnauthorized: false
+                    }
             });
 
-            if (response.status >= 200 && response.status < 300) {
+            const mailOptions = {
+                from: "Gold Bank",
+                to: email,
+                subject: 'Your Verification Code',
+                text: `Your verification code is: ${code}`,
+                html: `<p>Your verification code is: <b>${code}</b></p>` 
+            };
+
+            try {
+                await transporter.sendMail(mailOptions);
                 await client.setEx(email, 65, code);
 
                 console.log(`Verification code sent to ${email}`);
                 res.status(200).json({ message: 'Code sent successfully' });;
-            } else {
-                throw new Error('Failed to send verification code');
+            } catch(error: any) {
+                throw new Error(`Failed to send verification code: ${error}`);
             }
         } catch(error) {
             return next(error);
